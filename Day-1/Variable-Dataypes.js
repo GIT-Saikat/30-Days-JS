8364096469f3cb6aa065bc20d4952f4f2ea20d53
Saikat Mondal <136197192+GIT-Saikat@users.noreply.github.com>
2024-07-15 22:12:50 +0530

    Add files via upload

---
+++ Day-1/Variable-Dataypes.js
@@ -0,0 +1,46 @@
+//Task-1
+
+var number = 27;
+console.log(number);
+
+//Task-2
+
+let str ="Saikat";
+console.log(str);
+
+//Task-3
+
+const isValue="true";
+console.log(isValue);
+
+//Task-4
+
+let strr="Saikat Mondal";
+let num=21;
+const myArr= new Array(9,8,7,6,5);
+const isAlive=true;
+const user={
+    name:"Saikat",
+    age:19,
+    isAlive:"false"
+};
+
+console.log(typeof(strr));
+console.log(typeof(num));
+console.log(typeof(myArr));
+console.log(typeof(isAlive));
+console.log(typeof(user));
+
+//Task-5
+
+let balance=100000;
+console.log(balance);
+balance=200;
+console.log(balance);
+
+//Task-6
+
+const bal2=500;
+console.log(bal2);
+bal2=1000;
+console.log(bal2);    // TypeError: Assignment to constant variable.
